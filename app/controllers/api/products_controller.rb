0569e3d0371c3a25d8233107f48296f9a0dfb6be
lgonz053 <lgonz053@gmail.com>
2019-01-18 17:08:59 -0600

    create,show,destroy

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,41 @@
 class Api::ProductsController < ApplicationController
-  def display_information
-    @inventory = Product.all
-    render "inventory_view.json.jbuilder"
+  def index
+    @products = Product.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    product_id = params[:id]
+    @product = Product.find(product_id)
+    render 'show.json.jbuilder'
+  end
+
+  def create
+    @product = Product.new(
+                           name: params[:name],
+                           price: params[:price],
+                           image_url: params[:image_url],
+                           description: params[:description]
+                          )
+
+    @product.save
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+
+    @product.save
+    render 'show.json.jbuilder'
+  end
+
+  def delete
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {Message: "Successfully removed product"}
   end
 end
